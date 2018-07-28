838a8aa230c1c29fde98a77ff6c6f0e1f788dd89
microxuzh <michael@Michaels-MacBook-Pro.local>
2018-07-28 17:07:25 +0800

    Try Vue, a javascript framework.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return "hello world!!"
+    msg = 'Welcome to my Flask world!'
+    return render_template('index.html', title='', msg=msg)
+
+
+@app.route('/vue')
+def vue_handler():
+    vue_msg = 'Try Vue frame first!'
+    return render_template('vue.html', title='Vue - A javascript framework', msg=vue_msg)
